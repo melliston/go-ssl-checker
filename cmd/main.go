82368e2276a134b2cae2db4fdaa564a21df5eada
Matthew Elliston <mpgelliston@gmail.com>
2023-07-27 14:44:41 +0100

    Refactored the checking to output it in a more CSV friendly format for batch processing

--- cmd/main.go
+++ cmd/main.go
@@ -8,18 +8,34 @@ import (
 )
 
 func main() {
-	domain := "thegrandfathersteakhouse.uk"
-	conn, err := tls.Dial("tcp", domain+":443", nil)
-	if err != nil {
-		log.Fatalf("server does not support SSL certificate: %s", err.Error())
-	}
+	separator := ","
+	domains := []string{"thegrandfathersteakhouse.uk"}
 
-	err = conn.VerifyHostname(domain)
-	if err != nil {
-		log.Fatalf("hostname does not match the SSL certificate: %s", err.Error())
-	}
+	log.Printf("Domain%sValid%sExpiry%sSSL Issuer%sNotes", separator, separator, separator, separator)
+	for _, domain := range domains {
+		valid := true
+		notes := ""
+		conn, err := tls.Dial("tcp", domain+":443", nil)
+		if err != nil {
+			notes += fmt.Sprintf("server does not support SSL certificate: %s", err.Error())
+			valid = false
+		}
 
-	expiry := conn.ConnectionState().PeerCertificates[0].NotAfter
-	fmt.Printf("Domain: %s\nSSL issuer: %s\nExpiry: %v\n\n", domain, conn.ConnectionState().PeerCertificates[0].Issuer, expiry.Format(time.RFC850))
+		err = conn.VerifyHostname(domain)
+		if err != nil {
+			notes += fmt.Sprintf("hostname does not match the SSL certificate: %s", err.Error())
+			valid = false
+		}
+
+		expiry := conn.ConnectionState().PeerCertificates[0].NotAfter
+		if time.Now().After(expiry) {
+			notes += fmt.Sprintf("SSL certificate has expired: %v", expiry.Format(time.RFC850))
+			valid = false
+		}
+
+		log.Printf("%s%s%t%s%s%s%s%s%s\n", domain, separator, valid, separator, expiry.Format(time.RFC850), separator, conn.ConnectionState().PeerCertificates[0].Issuer, separator, notes)
+
+		conn.Close()
+	}
 
 }
